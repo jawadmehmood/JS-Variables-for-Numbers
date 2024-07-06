e8bf790a89f7a607c4b71caaf31097c3a0a43545
jawadmehmood <jawadam5678@gmail.com>
2024-07-07 03:54:32 +0500

    JS Variables for Numbers Completed

---
+++ app.js
@@ -0,0 +1,23 @@
+// Assignment 1
+
+var age = 23;
+alert('I am ' + age + ' years old');
+console.log('I am ' + age + ' years old');
+
+// Assignment 2
+
+var numOfVisits = 10;
+alert('You have visited this site ' + numOfVisits + ' times');
+console.log('You have visited this site ' + numOfVisits + ' times');
+
+// Assignment 3
+
+var birthYear = 2000;
+document.write('My birth year is ' + birthYear + '<br />' + 'Data type of my declared variable is number');
+
+// Assignment 4
+
+var fullName = 'John Doe';
+var product = ' T-shirt(s)';
+var quantity = 5;
+document.write('<br />' + fullName + ' ordered ' + quantity + product + ' on XYZ Clothing store');
